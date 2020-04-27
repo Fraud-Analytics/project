6b4acccfa1d7740e79d555eb948106f0e3a58897
HanYaodong <hanyaodongcn@hotmail.com>
2020-04-27 13:22:25 +0800

    Cross validate model scores

--- dev/Han_Yaodong/util.py
+++ dev/Han_Yaodong/util.py
@@ -10,7 +10,7 @@ from sklearn.utils import shuffle
 __all__ = ['load_data', 'plot_report', 'fdr', 'fdr_prob']
 
 
-def load_data(filename, test_size=0.2):
+def load_data(filename, test_size=0.2, oot_size=0.2):
     """Loads data from file and split into train and test data.
     Parameters
     ==========
@@ -20,10 +20,14 @@ def load_data(filename, test_size=0.2):
     df = pd.read_csv(filename)
     y = df['fraud_label']
     df.drop(columns=['fraud_label'], inplace=True)
+    x_train, x_oot, y_train, y_oot = train_test_split(
+            df, y, test_size=oot_size, shuffle=False)
+    if test_size == 0:
+        x_train, y_train = shuffle(x_train, y_train)
+        return x_train, None, x_oot, y_train, None, y_oot
     x_train, x_test, y_train, y_test = train_test_split(
-        df, y, test_size=test_size, shuffle=False)
-    x_train, y_train = shuffle(x_train, y_train)
-    return x_train, x_test, y_train, y_test
+            x_train, y_train, test_size=test_size)
+    return x_train, x_test, x_oot, y_train, y_test, y_oot
 
 
 def load_raw_data(filename, n_feature=25, test_size=0.2):
@@ -71,3 +75,4 @@ def fdr_prob(y, y_prob, cutoff=0.03):
     sorted_prob = np.asarray(sorted(zip(y_prob, y), key=lambda x: x[0], reverse=True))
     cutoff_bin = sorted_prob[0:int(len(y) * cutoff), 1:]
     return len(cutoff_bin[cutoff_bin == 1]) / num_fraud
+
